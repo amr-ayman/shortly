90e36c01746bc3f4f35385f42c04cfc25211e105
Amr Ayman <asoliman@youxel.com>
2020-06-06 22:19:05 +0200

    Add Call To Action Component, Statistics Component And Footer

--- src/app/core/components/main-layout/header/header.component.ts
+++ src/app/core/components/main-layout/header/header.component.ts
@@ -7,7 +7,7 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  isMenuOpen = true;
+  isMenuOpen = false;
   constructor() {
   }
 
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,9 +2,11 @@ import {NgModule} from '@angular/core';
 import {HomeComponent} from './pages/home/home.component';
 import {RouterModule} from '@angular/router';
 import {HomeRoutingModule} from './home-routing.module';
+import { CallToActionComponent } from './components/call-to-action/call-to-action.component';
+import { StatisticsComponent } from './components/statistics/statistics.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, CallToActionComponent, StatisticsComponent],
   imports: [
     RouterModule,
     HomeRoutingModule
